bb68ceddae352d4abac6183e2c739e1d42872559
gxe1157 <gx1157@yahoo.com>
2017-12-18 07:41:51 -0500

    fixed on clicked in store_catalog manage

--- public/js/site_datatable_loader.js
+++ public/js/site_datatable_loader.js
@@ -6,7 +6,7 @@
 $(document).ready(function() {
     $('#example').DataTable();
 
-    $('.btn-manage').on('click', function( ) {
+    $('.btn-manage.btn-danger').on('click', function( ) {
 		mess1='You are about to delete a record.\n\nPress Ok to Continue.';  
 		x = confirm(mess1);   
 		if (x == false ) {  
